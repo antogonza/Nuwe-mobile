2e88cc5c90b8dee2f623f962435fa6b7be046436
Antonio González <antogonzarodri1@gmail.com>
2021-07-20 13:06:49 +0200

    Pantalla de inicio finalizada y comienzo de login

--- nuwe/src/app/pages/login/login.page.ts
+++ nuwe/src/app/pages/login/login.page.ts
@@ -7,19 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-    usuario = {
-    email: '',
-    password: '',
-  };
-
   constructor() { }
 
   ngOnInit() {
   }
-
-   onSubmit(formulario: NgForm) {
-    console.log('Submit');
-    console.log(this.usuario);
-    console.log(formulario);
 
 }
